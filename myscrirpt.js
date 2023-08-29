b569c448740d9b5a342425884cf5d3a8a0fa1d92
GaboDzigo <gabodzigo@gmail.com>
2023-08-29 19:54:12 +0300

    w3school exercises

--- myscrirpt.js
+++ myscrirpt.js
@@ -1,7 +1,1 @@
-let x, y, z; // Statement1
-x = 5; // Statement2
-y = 6; // Statement3
-z = x + y; //Statement4
-
-document.getElementById("demo").innerHTML = 
-"The value of z is." + z + ".";
+document.getElementById('demo').innerHTML = "Hello Dolly.";
