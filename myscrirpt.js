de2757551f0549f8d5852a9926471042a219eec4
GaboDzigo <gabodzigo@gmail.com>
2023-08-30 14:13:21 +0300

    w3school exercises processing JavaScript Literals

--- myscrirpt.js
+++ myscrirpt.js
@@ -1,4 +1,1 @@
-function myFunction() {
-  document.getElementById('demo1').innerHTML = "Hello dolly!";
-  document.getElementById('demo2').innerHTML = "Hello how are you";
-}
+document.getElementById('demo').innerHTML = 10.50;
