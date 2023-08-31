108f4bbe925d7a007a0bec90cd72c9914b7d132a
GaboDzigo <gabodzigo@gmail.com>
2023-08-31 15:39:53 +0300

    js comments

--- myscrirpt.js
+++ myscrirpt.js
@@ -1,4 +1,7 @@
-// Change heading:
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+*/
 document.getElementById('myH').innerHTML = "JavaScript Comments";
-// Change paragraph:
 document.getElementById('myP').innerHTML = "My first paragraph.";
