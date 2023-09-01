78e0a49bd4d31c2527281af13b988eb339f337af
GaboDzigo <gabodzigo@gmail.com>
2023-09-01 15:03:24 +0300

    js variables

--- myscrirpt.js
+++ myscrirpt.js
@@ -1,4 +1,2 @@
-const price1 = 5 ;
-const price2 =  6 ;
-let  total  = price1 + price2;  
-document.getElementById('demo').innerHTML = "The total is: " + total;
+let carName = "Volvo";
+document.getElementById('demo').innerHTML = carName;
