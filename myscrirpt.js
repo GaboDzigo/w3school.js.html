4d48b02fb143fa1eeb02046e4a688909f7cce796
GaboDzigo <gabodzigo@gmail.com>
2023-09-02 18:45:03 +0300

    js const

--- myscrirpt.js
+++ myscrirpt.js
@@ -1,10 +1,3 @@
-const x = 10;
-// Here x is 10
-
-{
-  const x = 2;
-  // Here x is 2
-}
-
-// Here x is 10
-document.getElementById('demo').innerHTML = "x is " + x ;
+carName = "Volvo";
+document.getElementById('demo').innerHTML = carName;
+var carName;
