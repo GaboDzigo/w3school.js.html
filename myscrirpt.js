7e99c4bf942fb1fcb784734c655f20802925af83
GaboDzigo <gabodzigo@gmail.com>
2023-09-02 18:18:05 +0300

    js const

--- myscrirpt.js
+++ myscrirpt.js
@@ -1,8 +1,11 @@
-try{
-  const PI = 3.141592653589793;
-  PI = 3.14;
-} 
+// Create an Array:
+const cars = ["Saab", "Volvo", "BMW"];
 
- catch (err) {
- document.getElementById('demo').innerHTML = err ;
-}
+// Change an element:
+cars[0] = "Toyota";
+
+// Add an element:
+cars.push("Audi");
+
+// Display the Array:
+document.getElementById('demo').innerHTML = cars;
