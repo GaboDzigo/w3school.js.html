cc9b0b75dd25b6f84578469cf44a05d1f1681a0a
GaboDzigo <gabodzigo@gmail.com>
2023-09-02 18:12:55 +0300

    js const

--- myscrirpt.js
+++ myscrirpt.js
@@ -1,8 +1,8 @@
-try {
-   carName = Saab;
-   let carName = Volvo;
-}
+try{
+  const PI = 3.141592653589793;
+  PI = 3.14;
+} 
 
-catch(err) {
-  document.getElementById('demo').innerHTML = carName;
+ catch (err) {
+ document.getElementById('demo').innerHTML = err ;
 }
