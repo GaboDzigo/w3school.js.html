459c402d9e8c3e0ea23494d65c4bd4953adeb0ba
GaboDzigo <gabodzigo@gmail.com>
2023-09-03 15:53:47 +0300

    js operatos

--- myscrirpt.js
+++ myscrirpt.js
@@ -1,4 +1,4 @@
-let text1 = "20";
-let text2 = "5";
-let result = text1 < text2 ;
-document.getElementById('demo').innerHTML = "Is 20 less than 5?" +  result;
+let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
+document.getElementById('demo').innerHTML = text3;
