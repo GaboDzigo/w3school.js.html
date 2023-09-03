e0ab2b061b53275c6a3ac05186936aa183e871fd
GaboDzigo <gabodzigo@gmail.com>
2023-09-03 15:46:01 +0300

    js operators

--- myscrirpt.js
+++ myscrirpt.js
@@ -1,3 +1,4 @@
-var x = 10;
-x += 5;
-document.getElementById('demo').innerHTML = x;
+let text1 = "A";
+let text3 = "B";
+let result = text1 < text2 ;
+document.getElementById('demo').innerHTML = "is A less than B?" + result;
