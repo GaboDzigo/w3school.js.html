6789609383cc71aa03b2929cb72c0576f846e841
GaboDzigo <gabodzigo@gmail.com>
2023-09-03 15:27:07 +0300

    js operators

--- myscrirpt.js
+++ myscrirpt.js
@@ -1,2 +1,8 @@
-let x = 10;
-document.getElementById('demo').innerHTML = x;
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z
+let z = x + y;
+// Display z
+document.getElementById('demo').innerHTML = "The sum of x + y is:" + z;
