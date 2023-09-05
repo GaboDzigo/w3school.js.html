55439542e5a68f5857adc0812f1ba623200cd5df
GaboDzigo <gabodzigo@gmail.com>
2023-09-05 20:42:13 +0300

    js data type

--- myscript.js
+++ myscript.js
@@ -1,2 +1,7 @@
-const cars = ["Saab","Volvo","BMW"];
-document.getElementById('demo').innerHTML = cars[0];
+const person = {
+  firstName : "John",
+  lastName : "Doe",
+  age : 50,
+  eyeColor : "Blue"
+};
+document.getElementById('demo').innerHTML = person.firstName + " is " + person.age + " years old.";
