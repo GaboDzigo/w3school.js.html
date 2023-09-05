2c1ea6d127a9dac4327c4a4bce7a0eba6b0dee5f
GaboDzigo <gabodzigo@gmail.com>
2023-09-05 20:28:40 +0300

    js data type

--- myscript.js
+++ myscript.js
@@ -1,3 +1,2 @@
-let y = 123e5;
-let z = 123e-5;
-document.getElementById('demo').innerHTML = y + "<br>" + z;
+let x = BigInt("123456789012345678901234567890");
+document.getElementById('demo').innerHTML = x;
