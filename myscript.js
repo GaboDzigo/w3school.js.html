d1499fee48ed5121ba5996a809cd7780882a5cb4
GaboDzigo <gabodzigo@gmail.com>
2023-09-05 20:18:11 +0300

    js strings

--- myscript.js
+++ myscript.js
@@ -1,7 +1,4 @@
-let answer1 = "It's alright";
-let answer2 = "He is called 'Johnny'"
-let answer3 = 'He is called "Johnny"'
-document.getElementById('demo').innerHTML = 
-answer1 + "<br>" +
-answer2 + "<br>" +
-answer3;
+let x1 = 34.00;
+let x2 = 34;
+let x3 = 3.14;
+document.getElementById('demo').innerHTML = x1 + "<br>" + x2 + "<br>" + x3;
