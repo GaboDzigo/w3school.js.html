cb5d2806437ce66db8e3bd10f18cb7ab7dc629ef
GaboDzigo <gabodzigo@gmail.com>
2023-09-07 14:19:13 +0300

    js typeof

--- myscript.js
+++ myscript.js
@@ -1,4 +1,6 @@
-document.getElementById('demo').innerHTML = 
-typeof "" + "<br>" +
-typeof "John" + "<br>" + 
-typeof "John Doe";
+document.getElementById('demo').innerHTML =
+typeof 0 + "<br>" +
+typeof 314 + "<br>" +
+typeof 3.14 + "<br>" +
+typeof (3) + "<br>" +
+typeof (3 + 4) + "<br>" ;
