62d8cc434ebac03219f3dedae1332c7b054b7728
GaboDzigo <gabodzigo@gmail.com>
2023-09-07 14:14:10 +0300

    js typeof

--- myscript.js
+++ myscript.js
@@ -1,7 +1,4 @@
-const person = {
-  firstName : "John",
-  lastName : "Doe",
-  age : 50,
-  eyeColor : "Blue"
-};
-document.getElementById('demo').innerHTML = person.firstName + " is " + person.age + " years old.";
+document.getElementById('demo').innerHTML = 
+typeof "" + "<br>" +
+typeof "John" + "<br>" + 
+typeof "John Doe";
