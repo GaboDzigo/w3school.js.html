b49c8cb875b69239eb858c92ff94803c337287a5
GaboDzigo <gabodzigo@gmail.com>
2023-09-08 17:19:59 +0300

    js object

--- myscript.js
+++ myscript.js
@@ -1,5 +1,10 @@
 // Create an object:
-const person = {firstName:"John", lastName:"Doe", age:"50", eyecolor:"blue"}
+const person = {
+  firstName:"John",
+  lastName:"Doe",
+  age:"50",
+  eyeColor:"blue"
+};
 // Display some data from the object:
 document.getElementById('demo').innerHTML =
 person.firstName + " is " + person.age + " years old.";
