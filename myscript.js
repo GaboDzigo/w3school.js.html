ea5042707214a874f4a05493bfe22a2cfb8a7314
GaboDzigo <gabodzigo@gmail.com>
2023-09-09 18:13:21 +0300

    js string

--- myscript.js
+++ myscript.js
@@ -1,5 +1,2 @@
-let answer1 = "It's alright";
-let answer2 = "He is called 'Johnny'";
-let answer3 = 'He is called "Johnny"';
-document.getElementById('demo').innerHTML =
-answer1 + "<br>" + answer2 + "<br>" + answer3;
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.getElementById('demo').innerHTML = text.length;
