d7c966d867784813fc6dc9bb036f3d7b6b5e12de
GaboDzigo <gabodzigo@gmail.com>
2023-09-09 15:39:42 +0300

    js strings

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
-let carName1 = "Volvo XC60" // Double quotes
-let carName2 = 'Volvo XC60' // Single quotes
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
 document.getElementById('demo').innerHTML =
-carName1 + " " + carName2;
+answer1 + "<br>" + answer2 + "<br>" + answer3;
