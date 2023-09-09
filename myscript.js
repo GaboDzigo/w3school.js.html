936c3dff924c3a11ba427b79896ee587bcf0b2e9
GaboDzigo <gabodzigo@gmail.com>
2023-09-09 18:41:26 +0300

    js string

--- myscript.js
+++ myscript.js
@@ -1,2 +1,6 @@
-document.getElementById('demo').innerHTML = /
-"Hello Dolly!."
+// x is a string
+let x = "John";
+// y is an object
+let y = new string ("John");
+document.getElementById('demo').innerHTML=
+typeof x + "<br>" + typeof y;
