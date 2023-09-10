ed8f4390cf6bc3bfaf15f76b4f132344da1ae297
GaboDzigo <gabodzigo@gmail.com>
2023-09-10 14:20:53 +0300

    js string

--- myscript.js
+++ myscript.js
@@ -1,3 +1,3 @@
 let x = "John"; // x is a string
 let y = new String ("John"); // y is an object
-document.getElementById('demo').innerHTML = (x==y);
+document.getElementById('demo').innerHTML = (x===y);
