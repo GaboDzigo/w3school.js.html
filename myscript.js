64455e2ab999bd7932a21aa29b5119c514243cfc
GaboDzigo <gabodzigo@gmail.com>
2023-09-11 17:06:57 +0300

    js string method

--- myscript.js
+++ myscript.js
@@ -1,3 +1,5 @@
-let text =  "I love cats. Cats are very easy to love. Cats are very popular";
-text = text.replaceAll(/Cats/g,"Dogs")
-text = text.replaceAll(/cats/g,"dogs")
+function myFunction() {
+  let text = document.getElementById("demo").innerHTML;
+  document.getElementById('demo').innerHTML =
+  text.toUpperCase();
+}
