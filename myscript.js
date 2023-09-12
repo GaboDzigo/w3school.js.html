1d2966790f2e265e58d5f69cb767be930f871562
GaboDzigo <gabodzigo@gmail.com>
2023-09-12 20:38:08 +0300

    js string method

--- myscript.js
+++ myscript.js
@@ -1,4 +1,2 @@
-let text1 = "Hello"
-let text2 = "World!";
-let text3 = text1.concat(" ", text2);
-document.getElementById('demo').innerHTML = text3;
+let text1 = "     Hello World!     ";
+let text2 = text1.trimStart();
