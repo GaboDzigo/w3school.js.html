d71636bdee7ff40fe8b9e5d50c941ac5f950b044
GaboDzigo <gabodzigo@gmail.com>
2023-09-12 21:15:13 +0300

    js string method

--- myscript.js
+++ myscript.js
@@ -1,2 +1,4 @@
-let text = "HELLO WORLD";
-document.getElementById('demo').innerHTML = text[0];
+let text = "HELLO WORLD"
+
+text[0] = "A"; // Does not work
+document.getElementById('demo').innerHTML = text;
