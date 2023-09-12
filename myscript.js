fb9c33de4ffb2b542211aa1acd7e654f3b612829
GaboDzigo <gabodzigo@gmail.com>
2023-09-12 20:47:10 +0300

    js string method

--- myscript.js
+++ myscript.js
@@ -1,4 +1,2 @@
-let text1 = "     Hello World!     ";
-let text2 = text1.trimStart();
-document.getElementById('demo').innerHTML =
-"Length text1 =" + text1.length + "<br>Length text2" + text2.length;
+let text = "5";
+document.getElementById('demo').innerHTML = text.padStart(4,"x");
