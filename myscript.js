e3b220e4f397938b109e0992cec48105ac70dd6a
GaboDzigo <gabodzigo@gmail.com>
2023-09-14 21:35:51 +0300

    js string search

--- myscript.js
+++ myscript.js
@@ -1,4 +1,2 @@
-let text = "I love cats. Cats are very easy to love. Cats are very popular."
-const iterator = text.matchAll("Cats");
-
-document.getElementById('demo').innerHTML = Array.from(iterator);
+let text = "Hello world, welcome to the universe.";
+document.getElementById('demo').innerHTML = text.includes("world");
