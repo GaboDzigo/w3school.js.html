e1e1eb551626197e66b6a0f27886be58e98c2924
GaboDzigo <gabodzigo@gmail.com>
2023-09-16 14:11:04 +0300

    js Template Literals

--- myscript.js
+++ myscript.js
@@ -1,2 +1,2 @@
-let text = "Hello world, welcome to the universe.";
-document.getElementById('demo').innerHTML = text.endsWith("world", 11);
+let text = `Hello world!`
+document.getElementById('demo').innerHTML = text;
