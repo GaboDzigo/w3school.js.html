e26aa805403e2ed238f0bff0d6b9cf600a5b2a83
GaboDzigo <gabodzigo@gmail.com>
2023-09-19 14:16:21 +0300

    js numbers

--- myscript.js
+++ myscript.js
@@ -1,4 +1,3 @@
-let x = 10;
-let y = "20";
-let z = y + x;
-document.getElementById('demo').innerHTML = z;
+let x = "10";
+let y = 20;
+document.getElementById('demo').innerHTML = "The result is: " + x + y;
