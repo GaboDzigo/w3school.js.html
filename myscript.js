8015fedff2523433b88790ca2ebfd1094786cc84
GaboDzigo <gabodzigo@gmail.com>
2023-09-21 20:32:51 +0300

    js numbers

--- myscript.js
+++ myscript.js
@@ -1,7 +1,3 @@
-let myNumber = 2;
-let txt = " ";
-while(myNumber != Infinity) {
-  myNumber = myNumber * myNumber
-  txt = txt + myNumber + "<br>"
-}
-document.getElementById('demo').innerHTML = txt;
+let x = 2/0;
+let y = - 2/0;
+document.getElementById('demo').innerHTML = x + "<br>" + y;
