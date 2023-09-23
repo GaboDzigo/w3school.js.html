8f33c2121211965919599ada0ed884204b8eb575
GaboDzigo <gabodzigo@gmail.com>
2023-09-23 15:15:40 +0300

    js numbers

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
-// x is a number
-let x = 500;
+// x is an object
+let x = new Number(500);
 
 // y is an object
 let y = new Number(500);
-document.getElementById('demo').innerHTML = (x===y);
+document.getElementById('demo').innerHTML = (x==y);
