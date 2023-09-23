8807959de72893d72a4fa92f3b3fb4bd7989908c
GaboDzigo <gabodzigo@gmail.com>
2023-09-23 16:20:42 +0300

    BigInt

--- myscript.js
+++ myscript.js
@@ -1,6 +1,3 @@
-// x is an object
-let x = new Number(500);
-
-// y is an object
-let y = new Number(500);
-document.getElementById('demo').innerHTML = (x==y);
+let x = 999999999999999;
+let y = 9999999999999999;
+document.getElementById('demo').innerHTML = x + "<br>" + y;
