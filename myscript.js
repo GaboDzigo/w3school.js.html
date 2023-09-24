b0f0a3c65839ce434be39509543631d4d9f0d8a8
GaboDzigo <gabodzigo@gmail.com>
2023-09-24 14:16:42 +0300

    js BigInt

--- myscript.js
+++ myscript.js
@@ -1,3 +1,2 @@
-let x = 9999999999999999;
-let y = BigInt("9999999999999999");
-document.getElementById('demo').innerHTML = x + "<br>" + y;
+let x = ("9999999999999999");
+document.getElementById('demo').innerHTML = typeof x;
