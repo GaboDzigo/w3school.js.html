1fd93a697bbf2be3cf6c16ec1f4ee5f6582a56d9
GaboDzigo <gabodzigo@gmail.com>
2023-09-24 18:53:58 +0300

    js BigInt

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 let x = 9.656;
 document.getElementById('demo').innerHTML = 
-x.toExponential() + "<br>" +
-x.toExponential(2) + "<br>" +
-x.toExponential(4) + "<br>" +
-x.toExponential(6);
+x.toFixed(0) + "<br>" +
+x.toFixed(2) + "<br>" +
+x.toFixed(4) + "<br>" +
+x.toFixed(6);
