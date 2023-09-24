b11ae83dfe6eaa8f2d80d2d1e7cbafe7415d39d4
GaboDzigo <gabodzigo@gmail.com>
2023-09-24 14:28:55 +0300

    js BigInt

--- myscript.js
+++ myscript.js
@@ -1,3 +1,4 @@
-let x = 5n;
-let y = Number(x) / 2;
-document.getElementById('demo').innerHTML =y;
+let hex = 0x20000000000003n;
+let oct = 0o400000000000000003n;
+let bin = 0b100000000000000000000000000000000000000000000000000011n;
+document.getElementById('demo').innerHTML = hex + "<br>" + oct + "<br>" + bin;
