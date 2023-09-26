963046ca9ac72f8e97d15becb04c5f5d055270d6
GaboDzigo <gabodzigo@gmail.com>
2023-09-26 19:30:03 +0300

    js number methods

--- myscript.js
+++ myscript.js
@@ -1,8 +1,6 @@
-document.getElementById('demo').innerHTML =
-parseInt("-10") + "<br>" +
-parseInt("-10.33") + "<br>" +
-parseInt("10") + "<br>" +
-parseInt("10.33") + "<br>" +
-parseInt("10 6") + "<br>" +
-parseInt("10 years") + "<br>" +
-parseInt("years 10") ;
+document.getElementById('demo').innerHTML = 
+parseFloat("10") + "<br>" +
+parseFloat("10.33") + "<br>" +
+parseFloat("10 6") + "<br>" +
+parseFloat("10 years") + "<br>" +
+parseFloat("years 10");
