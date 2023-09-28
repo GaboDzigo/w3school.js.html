64f96d43525b92015cfb5968603b5b5aa1302b72
GaboDzigo <gabodzigo@gmail.com>
2023-09-28 21:10:00 +0300

    js arrays

--- myscript.js
+++ myscript.js
@@ -1,3 +1,2 @@
-//const points = new Array(40, 100, 1, 5, 25, 10);
-const ponits = [40, 100, 1, 5, 25, 10];
-document.getElementById('demo').innerHTML = points[0];
+var points = new Array(40, 100, 1);
+document.getElementById('demo').innerHTML = points;
