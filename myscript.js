13f638cd9797946bfaf647918aef07d1dbe5722b
GaboDzigo <gabodzigo@gmail.com>
2023-09-29 20:17:45 +0300

    js arrays

--- myscript.js
+++ myscript.js
@@ -1,2 +1,2 @@
-var fruits = ["Banana","Orange","Apple"];
-document.getElementById('demo').innerHTML = fruits instanceof Array;
+const fruits = ["Banana","Orange","Apple","Mango"];
+document.getElementById('demo').innerHTML = fruits.toString;
