e55fb17b955d558124da6aecf971ae4c09fe028b
GaboDzigo <gabodzigo@gmail.com>
2023-09-29 20:31:59 +0300

    js method arrays

--- myscript.js
+++ myscript.js
@@ -1,2 +1,4 @@
-const fruits = ["Banana","Orange"," Apple","Mango"];
-document.getElementById('demo').innerHTML = fruits.join(" * ");
+const fruits = ["Banana","Orange","Apple","Mango"]
+document.getElementById('demo1').innerHTML = fruits;
+fruits.pop();
+document.getElementById('demo2').innerHTML = fruits;
