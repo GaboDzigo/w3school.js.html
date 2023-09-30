63aee8f3f454f847ccb1bf0d9904b340460c7205
GaboDzigo <gabodzigo@gmail.com>
2023-09-30 17:15:40 +0300

    js method arrays

--- myscript.js
+++ myscript.js
@@ -1,5 +1,7 @@
-const myGirls = ["Cecelie", "Lone"];
-const myBoys = ["Emil","Tobias","Linus"];
-const myChildren = myGirls.concat(myBoys);
+const array1 = ["Cecilie", "Lone"];
+const array2 = ["Emil","Tobias","Linus"];
+const array3 = ['Robin',"Morgan"];
 
-document.getElementById('demo').innerHTML = myChildren;
+ const myChildren = array1.concat(array2,array3);
+
+ document.getElementById('demo').innerHTML = myChildren;
