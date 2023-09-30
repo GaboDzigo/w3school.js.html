28fafb9a94e178b3182e0ea361258aea5f6ad923
GaboDzigo <gabodzigo@gmail.com>
2023-09-30 16:53:13 +0300

    js method arrays

--- myscript.js
+++ myscript.js
@@ -1,4 +1,9 @@
 const fruits = ["Banana","Orange","Apple","Mango"];
-document.getElementById('demo1').innerHTML = fruits;
-fruits[fruits.length] = "Kiwi";
-document.getElementById('demo2').innerHTML = fruits;
+
+document.getElementById('demo1').innerHTML = 
+"The First fruit is : " + fruits[0];
+
+delete fruits[0];
+
+document.getElementById('demo2').innerHTML = 
+"The First fruit is : " + fruits[0];
