717f9cccef60259e01446d78706fcf44bf898b6d
GaboDzigo <gabodzigo@gmail.com>
2023-10-01 20:03:04 +0300

    js arrays Methods

--- myscript.js
+++ myscript.js
@@ -1,3 +1,3 @@
 const fruits = ["Banana","Orange","Apple","Mango"];
-const citrus =fruits.sliced(1);
+const citrus = fruits.slice(1);
 document.getElementById('demo').innerHTML = fruits + "<br><br>" + citrus;
