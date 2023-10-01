df55c7ac63c71effb862e8a45371615891c2a60f
GaboDzigo <gabodzigo@gmail.com>
2023-10-01 20:31:10 +0300

    js methods

--- myscript.js
+++ myscript.js
@@ -1,3 +1,2 @@
-const fruits = ["Banna","Orange","Apple","Mango"];
-const citrus = fruit.slice(2);
-document.getElementById('demo').innerHTML = fruits + "<br><br>" + citrus;
+const fruits = ["banana","Apple","Orange","Mango"];
+document.getElementById('demo').innerHTML = fruits.toString;
