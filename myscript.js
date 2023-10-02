a71fc6025bd722f24adafeacf52a6a2e1873c85a
GaboDzigo <gabodzigo@gmail.com>
2023-10-02 20:00:49 +0300

    js array sort

--- myscript.js
+++ myscript.js
@@ -1,2 +1,5 @@
-const fruits = ["Banana","Orange","Apple","Mango"];
-document.getElementById('demo').innerHTML = fruits;
+const fruits = ["Banana","orange","Apple","Mango"];
+document.getElementById('demo1').innerHTML = fruits;
+
+fruits.sort();
+document.getElementById('demo2').innerHTML = fruits;
