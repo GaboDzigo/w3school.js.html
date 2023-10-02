c196815abba47e0670b61644ea124781573961eb
GaboDzigo <gabodzigo@gmail.com>
2023-10-02 20:09:37 +0300

    js array sort

--- myscript.js
+++ myscript.js
@@ -1,5 +1,11 @@
-const fruits = ["Banana","orange","Apple","Mango"];
+// Create and display an array:
+const fruits = ["Banana","Orange","Apple","Mango"];
 document.getElementById('demo1').innerHTML = fruits;
 
+// First sort the array
 fruits.sort();
+
+// Then reverse it:
+fruits.reverse();
+
 document.getElementById('demo2').innerHTML = fruits;
