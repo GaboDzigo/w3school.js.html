49ae4b01edb6ab7c5d4555d43661bfcf86caf323
GaboDzigo <gabodzigo@gmail.com>
2023-10-04 19:08:15 +0300

    js arrays sort

--- myscript.js
+++ myscript.js
@@ -1,12 +1,7 @@
 const points = [40,100,1,5,25,10];
-document.getElementById('demo').innerHTML = points;
+document.getElementById(demo).innerHTML = points;
 
- function  myFunction1() {
- points.sort()
- document.getElementById('demo').innerHTML = points;
- }
-
- function  myFunction2() {
-  points.sort(function (a, b){return a - b});
+function myFunction() {
+  points.sort(function(){return 0.5 - Math.random()});
   document.getElementById('demo').innerHTML = points;
-  }
+}
