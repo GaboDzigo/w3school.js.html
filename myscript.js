6d0222320548050c9a43f86d00be2f0bca6bc21c
GaboDzigo <gabodzigo@gmail.com>
2023-10-07 14:39:50 +0300

    js array sort

--- myscript.js
+++ myscript.js
@@ -6,11 +6,19 @@ const cars = [
 
 displayCars();
 
-cars.sort(function(a, b){return a.year - b.year});
-displayCars();
+function myFunction() {
+  cars.sort(function(a, b){
+    let x = a.type.toLowerCase();
+    let y = b.type.toLowerCase();
+    if (x < y) {return -1;}
+    if (x > y) {return 1;}
+    return 0;
+  });
+  displayCars();
+}
 
 function displayCars() {
-  document.getElementById("demo").innerHTML = 
+  document.getElementById("demo").innerHTML =
   cars[0].type + " " + cars[0].year + "<br>" +
   cars[1].type + " " + cars[1].year + "<br>" +
   cars[2].type + " " + cars[2].year;
