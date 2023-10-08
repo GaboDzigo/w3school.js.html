1da22864530750d2b68dc552b3377f338493308a
GaboDzigo <gabodzigo@gmail.com>
2023-10-08 18:14:35 +0300

    js array iteration

--- myscript.js
+++ myscript.js
@@ -1,9 +1,8 @@
-const numbers = [45, 4, 9, 16, 25];
-let sum = numbers.reduce(myFunction);
+const numbers = [45,4,9,16,25];
+const sum = numbers.reduce(myFunction);
 
-document.getElementById('demo').innerHTML = "The sum is" + sum;
+document.getElementById("demo").innerHTML = "The sum is " + sum;
 
-function myFunction(total, value, index, array) {
+function myFunction(total,value) {
   return total + value;
 }
-
