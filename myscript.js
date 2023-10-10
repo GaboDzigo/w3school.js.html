0e7c339a87b549da9a315f4c7c1c540b57b1a67e
GaboDzigo <gabodzigo@gmail.com>
2023-10-10 20:28:09 +0300

    js array iteration

--- myscript.js
+++ myscript.js
@@ -1,8 +1,5 @@
-const numbers = [45,4,9,16,25];
-const someOver18 =  numbers.some(myFunction);
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+let position = fruits.indexOf("Apple") + 1;
 
-document.getElementById('demo').innerHTML = "Some is over 18 " + someOver18;
 
-function myFunction(value, index,array) {
-  return value > 18;
-}
+document.getElementById('demo').innerHTML = "Apple is found in position " + position;
