bd164917a918a34ee8ed3bb49c6ab8f637c98528
GaboDzigo <gabodzigo@gmail.com>
2023-10-10 20:35:05 +0300

    js array iteration

--- myscript.js
+++ myscript.js
@@ -1,5 +1,4 @@
-const fruits = ["Apple", "Orange", "Apple", "Mango"];
+const fruits = ["Apple","Orange","Apple","Mango"];
 let position = fruits.indexOf("Apple") + 1;
 
-
-document.getElementById('demo').innerHTML = "Apple is found in position " + position;
+document.getElementById('demo').innerHTML = "apple is found in position" + position;
