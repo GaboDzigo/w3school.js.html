54265da7a0e057d4e312ef0f642a5537d53b3b1e
GaboDzigo <gabodzigo@gmail.com>
2023-10-11 20:24:59 +0300

    js array iteration

--- myscript.js
+++ myscript.js
@@ -1,2 +1,7 @@
-const fruits = ["Banana","Orange","Apple","Mango"];
-document.getElementById('demo').innerHTML = fruits.include("Mango");
+const q1 = ["Jan","Feb","Mar"]
+const q2 = ["Apr","May","jun"]
+const q3 = ["Jul","Aug","Sept"]
+const q4 = ["Oct","Nov","May"]
+
+const year = [...q1,...q2,...q3,...q4];
+document.getElementById('demo').innerHTML = year;
