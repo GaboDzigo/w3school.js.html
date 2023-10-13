cdc73c6a687beb5558a40247378deaf35512c066
GaboDzigo <gabodzigo@gmail.com>
2023-10-13 21:45:50 +0300

    js array const

--- myscript.js
+++ myscript.js
@@ -1,7 +1,2 @@
-const q1 = ["Jan","Feb","Mar"]
-const q2 = ["Apr","May","jun"]
-const q3 = ["Jul","Aug","Sept"]
-const q4 = ["Oct","Nov","May"]
-
-const year = [...q1,...q2,...q3,...q4];
-document.getElementById('demo').innerHTML = year;
+const cars = ["Saab","Volvo","BMW"]
+document.getElementById('demo').innerHTML = cars;
