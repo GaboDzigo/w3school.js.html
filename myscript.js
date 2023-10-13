ca91856ed8500872d5e8d5e9c40b80f7478f8240
GaboDzigo <gabodzigo@gmail.com>
2023-10-13 22:27:05 +0300

    js array const

--- myscript.js
+++ myscript.js
@@ -1,4 +1,8 @@
-cars = ["Saab","Volvo","BMW"];
-var cars;
-
-document.getElementById('demo').innerHTML = cars[0];
+var cars = ["Saab","Volvo","BMW"];
+// Here cars[0] is "Saab"
+{
+  var cars = ["Toyota","Volvo","BMW"];
+    // Here cars[0] is "Toyota"
+}
+  // Here cars[0] is "Toyota"
+  document.getElementById("demo").innerHTML = cars[0];
