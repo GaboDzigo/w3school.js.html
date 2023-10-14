0b93b4b11b9f0dd3530367634e8e03c82888482b
GaboDzigo <gabodzigo@gmail.com>
2023-10-14 10:19:45 +0300

    js date

--- myscript.js
+++ myscript.js
@@ -1,8 +1,2 @@
-var cars = ["Saab","Volvo","BMW"];
-// Here cars[0] is "Saab"
-{
-  var cars = ["Toyota","Volvo","BMW"];
-    // Here cars[0] is "Toyota"
-}
-  // Here cars[0] is "Toyota"
-  document.getElementById("demo").innerHTML = cars[0];
+const d = new Date();
+document.getElementById('demo').innerHTML = d;
