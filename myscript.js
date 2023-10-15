4353ee5856f1364b0110f9d8364a1a1db098b2ec
GaboDzigo <gabodzigo@gmail.com>
2023-10-15 14:49:04 +0300

    js get data methods

--- myscript.js
+++ myscript.js
@@ -1,3 +1,2 @@
-let msec = new Date("March 21 2012");
-const d = new Date(msec);
-document.getElementById('demo').innerHTML = d;
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
