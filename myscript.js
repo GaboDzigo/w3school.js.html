8893ae732d33090e09497d5011b2112d30b1defe
GaboDzigo <gabodzigo@gmail.com>
2023-10-15 15:20:29 +0300

    js get data methods

--- myscript.js
+++ myscript.js
@@ -1,7 +1,6 @@
-const months = 
-["January","February","March","April","May","June","July",
+const months = ["January","February","March","April","May","June","July",
 "August","September","October","November","December"];
 
-const d = new Date();
-let month = months[d.getMonth()];
+const d = new Date("2021-03-25")
+const month = months[d.getMonth()];
 document.getElementById('demo').innerHTML = month;
