d5a6edfc2311956dd91065a306db88de15e69d02
GaboDzigo <gabodzigo@gmail.com>
2023-10-15 17:49:27 +0300

    js data get methods

--- myscript.js
+++ myscript.js
@@ -1,1 +1,2 @@
-const d = new Date("2021-03-25"); document.getElementById("demo").innerHTML = d.getHours();
+const d = new Date("2021-03-25");
+document.getElementById('demo').innerHTML = d.getMinutes;
