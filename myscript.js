f679e392e05fdc7ffa381886e7d2e98bf62bac5f
GaboDzigo <gabodzigo@gmail.com>
2023-10-15 17:40:12 +0300

    js date methods

--- myscript.js
+++ myscript.js
@@ -1,6 +1,2 @@
-const months = ["January","February","March","April","May","June","July",
-"August","September","October","November","December"];
-
-const d = new Date("2021-03-25")
-const month = months[d.getMonth()];
-document.getElementById('demo').innerHTML = month;
+const d = new Date("2021-03-25");
+document.getElementById("demo").innerHTML = d.getDate();
