103df48b8d79e26b849b27dba71ffa928885a906
GaboDzigo <gabodzigo@gmail.com>
2023-10-16 23:17:18 +0300

    js data get methods

--- myscript.js
+++ myscript.js
@@ -1,2 +1,9 @@
-const d = new Date();
-document.getElementById('demo').innerHTML = d.getTime();
+// Calculate milliseconds in a year
+const minute = 100 * 6;
+const hour = minute * 60;
+const day = hours * 24;
+const year = day * 365;
+
+// Divide Date.now() with a year
+let years = Math.round(Date.now() / year);
+document.getElementById('demo').innerHTML = years
