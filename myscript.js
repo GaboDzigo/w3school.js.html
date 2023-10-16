ef94e6fb12b7d28d27257ebc9ba9120d8c2e8860
GaboDzigo <gabodzigo@gmail.com>
2023-10-16 22:25:44 +0300

    js date get methods

--- myscript.js
+++ myscript.js
@@ -1,2 +1,5 @@
-const d = new Date();
-document.getElementById('demo').innerHTML = d.getDay();
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Saturday"]
+
+const d = new Date("2021-03-25");
+let day = day[d.getDay()];
+document.getElementById('demo').innerHTML = day;
