8f314d43e4b03742128a8e6dd90dc11614b30769
GaboDzigo <gabodzigo@gmail.com>
2023-10-17 21:09:08 +0300

    js set date methods

--- myscript.js
+++ myscript.js
@@ -1,3 +1,9 @@
-const d = new Date();
-d.setSeconds(30);
-document.getElementById("demo").innerHTML = d;
+let text;
+const today = new Date();
+const someday =  new Date();
+someday.setFullYear(2100, 0, 14);
+if ( someday > today) {
+  text = "Today is before january 2100.";
+} else {
+  text = "Today is after january 2100.";
+}
