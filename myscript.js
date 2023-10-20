0e44b443eac29815e5ca761f95aa0e72cb33b620
GaboDzigo <gabodzigo@gmail.com>
2023-10-20 18:08:43 +0300

    js boolean

--- myscript.js
+++ myscript.js
@@ -1,4 +1,3 @@
-let x = false;// x is a boolean
-let y = new Boolean(false);// y is an object
-
-document.getElementById("demo").innerHTML = (x===y);
+const x = new Boolean(false);
+const y = new Boolean(false);
+document.getElementById("demo").innerHTML = (x==y);
