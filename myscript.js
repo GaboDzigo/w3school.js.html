49f9a7479efbac46148dc062ba18ead69bb0b492
GaboDzigo <gabodzigo@gmail.com>
2023-10-21 19:26:03 +0300

    js comparisons

--- myscript.js
+++ myscript.js
@@ -1,6 +1,1 @@
-function myFunction() {
-  let age = document.getElementById("age").value;
-  let voteable = (age < 18) ? "Too young":"Old enough";
-  document.getElementById("demo").innerHTML = voteable + "to vote";
-}
-
+document.getElementById("demo").innerHTML = 2 < 12;
