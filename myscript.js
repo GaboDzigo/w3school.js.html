61dfde13b0d71b626e5bc2f730c108bdf41148a2
GaboDzigo <gabodzigo@gmail.com>
2023-10-21 19:47:42 +0300

    js compasisons

--- myscript.js
+++ myscript.js
@@ -1,12 +1,4 @@
-function myFunction() {
-let voteable;
-let age = Number(document.getElementById("age").value);
-if (isNaN(age)) {
-  voteable = "Input is not a number";
-} else {
-  voteable = (age < 18) ? "Too young to vote" : "Old enough to vote"
-}
-document.getElementById("demo").innerHTML = voteable + "to vote";
-}
-
-
+let name = null;
+let text = "missing";
+let result = name ?? text;
+document.getElementById("demo").innerHTML = "The name is " + name;
