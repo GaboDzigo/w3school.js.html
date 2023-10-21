7ccd8b9443b499508162840c3a43757b05064dd1
GaboDzigo <gabodzigo@gmail.com>
2023-10-21 18:07:32 +0300

    js comparisons

--- myscript.js
+++ myscript.js
@@ -1,8 +1,6 @@
 let x = 6;
 let y = 3;
 
-document.getElementById("demo").innerHTML = 
-(x == 5 || y == 5 ) +  "<br>"  +
-(x == 6 || y == 0 ) +  "<br>"  +
-(x == 0 || y == 3 ) +  "<br>"  +
-(x == 6 || y == 3 );
+document.getElementById("demo").innerHTML =
+!(x === y) + "<br>" +
+!(x > y);
