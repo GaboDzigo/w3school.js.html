b0b68784961e89067345fe5366e5045a082f1482
GaboDzigo <gabodzigo@gmail.com>
2023-10-23 17:13:22 +0300

    js for loop

--- myscript.js
+++ myscript.js
@@ -1,9 +1,6 @@
-const cars = ["BMW", "Volvo", "Saab", "Ford" , "Fiat" , "Audi"];
-
 let text = "";
-for (let i = 0; i < cars.length; i++) {
-  text += cars[i] + "<br>";
+
+for (let i = 0; i < 5; i++) {
+  text += "The number is " + i + "<br>";
 }
-
 document.getElementById("demo").innerHTML = text;
-
