fcd8cc68a17e0ff240037cdaf05011f4fbdf1b75
GaboDzigo <gabodzigo@gmail.com>
2023-10-23 14:32:48 +0300

    js switch

--- myscript.js
+++ myscript.js
@@ -1,16 +1,13 @@
-let text;
-switch (new Date().getHours()) {
-  case 4 :
-  case 5 :
-    text = "Soon it is Weekend";
-   break;
-    
-   case 0 :
-   case 6 :
-    text = "It is Weekend";
-    break;
-    default:
-      text = "Looking forward to the Weekend";
-}
+let text = "0";
 
+switch (x) {
+  case 0 :
+  text = "Off";
+  break;
+  case 1 :
+    text = "On";
+    break;
+    default :
+    text = "No value found";
+}
 document.getElementById("demo").innerHTML = text;
