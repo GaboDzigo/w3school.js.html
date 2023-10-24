d33d99a20a359319273e77cf0b341d229ea11f61
GaboDzigo <gabodzigo@gmail.com>
2023-10-24 14:12:48 +0300

    js loop for

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
-var i = 5;
-for (var i = 0; i < 10; i++) {
-  //some statements
+let i = 5;
+for (let i = 0; i < 10; i++) {
+    // some statements
 }
 
 document.getElementById("demo").innerHTML = i;
